50deb763db74306f84f619753ff3fc437d71aed0
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-20 10:13:28 +0200

    Añadido Javado a la clase PrinResultados

--- src/examenfinalparte1/PrinResultados.java
+++ src/examenfinalparte1/PrinResultados.java
@@ -5,15 +5,25 @@ import java.io.IOException;
 public class PrinResultados {
 
     public static void main(String[] args) throws IOException {
-       
+       /**
+        * Creaundo un objeto de tipo MetodosCalculos llamamos a los metodos de esa clase.
+        * En el primer caso llamamos a SumaVariablesDistintas, pasandole dos parametros hará la suma entre ambos.
+        * El System.out.print nos mostrara el resultado.
+        */
         MetodosCalculos obj = new MetodosCalculos();
         int n = obj.SumaVariablesDistintas(10,20);
         System.out.println("Es: " + n);
-       
+       /**
+         * Llamando al metodo SumaMismaVariable realizará un calculo enviandole una unica variable.
+         * El System.out.print nos mostrara el resultado.
+         */
         MetodosCalculos nm = new MetodosCalculos();
         int m = nm.SumaMismaVariable(10);
         System.out.println("Es: " + m);
-        
+        /**
+         * Llamando a el metodo VariableMenosUno asignaremos el valor -1 a la variale int nn.
+         * El System.out.print nos mostrara el resultado.
+         */
         MetodosCalculos mn = new MetodosCalculos();
         int nn = mn.VariableMenosUno();
         System.out.println("Es: " + nn);
