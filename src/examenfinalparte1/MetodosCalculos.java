c44fbefc74194af2b090c687c4ea328bc8bd74d1
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-20 10:00:42 +0200

    Renombrados y cambiados de tipo(int) los Metodos Clase MetodosCalculos

--- src/examenfinalparte1/MetodosCalculos.java
+++ src/examenfinalparte1/MetodosCalculos.java
@@ -9,29 +9,33 @@ public class MetodosCalculos {
      * @param m variable de tipo int
      * @param j variable de tipo int
      */
-    public MetodosCalculos(int j, int m) {
+    
+    public int SumaVariablesDistintas(int j, int m){
         i = Suma(j, m);
+        return i;
     }
  /**
   * Pasandole unicamente el parametro j realizará la operación del metodo Suma, sumará j+j.
   * Recoge el resultado de la operación en la variable i.
   * @param j variable de tipo int
   */
-    public MetodosCalculos(int j) {
+   public int SumaMismaVariable(int j){
         i = Suma(j, j);
+        return i;
     }
  /**
   * La variable i,valdrá -1.
   */
-    public MetodosCalculos() {
+   public int VariableMenosUno() {
         i = -1;
+        return i;
     }
 /**
      * Método que contiene la operación principal, a partir de este método realizaremos los demás.
      * Realiza una suma mediante 2 variable x y x1.
      * @param x variabe de tipo int
      * @param x1 variable de tipo int
-     * @return la suma
+     * @return la suma de x+x1
      */
     public int Suma(int x, int x1) {
         return x + x1;
