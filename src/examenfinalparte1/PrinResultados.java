6a181f9e954596c0a9a6eb8018cf3a0bc5a7f19f
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-20 10:07:00 +0200

    Renombrados Metodos clase PrinResultados

--- src/examenfinalparte1/PrinResultados.java
+++ src/examenfinalparte1/PrinResultados.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 public class PrinResultados {
 
     public static void main(String[] args) throws IOException {
-        MetodosCalculos obj = new MetodosCalculos(10, 20);
-        int n = obj.i;
+       
+        MetodosCalculos obj = new MetodosCalculos();
+        int n = obj.SumaVariablesDistintas(10,20);
         System.out.println("Es: " + n);
-        MetodosCalculos nm = new MetodosCalculos(10);
-        int m = nm.i;
+       
+        MetodosCalculos nm = new MetodosCalculos();
+        int m = nm.SumaMismaVariable(10);
         System.out.println("Es: " + m);
+        
         MetodosCalculos mn = new MetodosCalculos();
-        int nn = mn.i;
+        int nn = mn.VariableMenosUno();
         System.out.println("Es: " + nn);
+        
     }
 }
